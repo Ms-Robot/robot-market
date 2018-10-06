8a751c01cf53a019080f28ec63175105eea64402
“Sandra <john.doe@nl.ibm.comclear>
2018-10-06 15:47:43 +0200

    add robotos table component

---
+++ src/app/robot-table/robot-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-robot-table',
+  templateUrl: './robot-table.component.html',
+  styleUrls: ['./robot-table.component.css']
+})
+export class RobotTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
